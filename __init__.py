ee8bc47d0303a1f66cc78becaf8d5d8f14ca2cc7
Marco Herrero <me@marhs.de>
2015-04-17 18:36:09 +0200

    Movido la app de flask al directorio principal

--- __init__.py
+++ __init__.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+import BDFuntions as bd
 
 app = Flask(__name__, template_folder=".")
 
 @app.route("/")
 def mapview():
     # creating a map in the view
+    print(bd.login('helmetk','topo'))
+    print(bd.login('helmetk','sadf'))
     return render_template('templates/map.html')
 
 if __name__ == "__main__":
