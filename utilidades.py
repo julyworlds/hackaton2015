14410aa2c147b2df26eefa850a94104060ae330d
aceconhielo <aceconhielo@gmail.com>
2015-04-17 17:31:46 +0200

    utilidades
    Funcion menordistancia y redifinida la getPuntosCercanos

--- utilidades.py
+++ utilidades.py
@@ -1,32 +1,37 @@
 import math
 
+#Sacar cosas cercas del punto inicio y punto fin 
+def menorDistancia(Punto1, Punto2, distancia):
+	(x1,y1)= Punto1
+	(x2,y2)= Punto2
+	
+	latitudDif= Punto2[0]-Punto1[0]
+	longitudRadio = Punto2[1]-Punto1[1]
 
-		
+	latitudDifMetros= (latitudDif*111)*1000
+	longitudRadioMetros= (longitudRadio*111)*1000
+	hip = math.sqrt( (latitudDifMetros)*(latitudDifMetros)+(longitudRadioMetros)*(longitudRadioMetros))
+	hipKm =hip/1000
+	if hipKm <= distancia:
+		return True 
+	
 def getPuntosCercanos(Punto,distancia, ListaSitios):
-	(x,y) = Punto 
+	#(x,y) = Punto 
 
 	PuntosCercanosList= list()
-	
-	for e in ListaSitios: 
-		
-		latitudRadio = e[3] - Punto[0]
-		longitudRadio = e[4] - Punto[1]
 
-		latitudMetros = (latitudRadio*111)*1000
-		print("lat",latitudMetros)
-		longtudMetris = (longitudRadio*111)*1000
-
-		hip = math.sqrt( (latitudMetros)*(latitudMetros)+(longtudMetris)*(longtudMetris))
-		hipKm =hip/1000
-		print("hip",hipKm)
-		if hipKm < distancia: 
-			PuntosCercanosList.append(e)
+	for (Id,Tipo,Nombre,la,lon) in ListaSitios:
+		if(menorDistancia((la,lon),Punto,distancia)):
+			PuntosCercanosList.append([Id,Tipo,Nombre,la,lon])
 
 	return PuntosCercanosList
 
 
 
 
-# Lista= [[1,"Biblioteca","Biblioteca1", 37.357924, -5.986320], [2,"Museo","Museo1",37.362922, -5.975878]]
-# PuntoA = (37.355508, -5.987698)
-# print(getPuntosCercanos(PuntoA,1,Lista))
+
+
+
+Lista= [[1,"Biblioteca","Biblioteca1", 37.357924, -5.986320], [2,"Museo","Museo1",37.362922, -5.975878]]
+PuntoA = (37.355508, -5.987698)
+print(getPuntosCercanos(PuntoA,50,Lista))
